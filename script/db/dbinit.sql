65a825e8fe686a83ba3e5897d8ddb150f8ee4e1d
魏冰涛 <13156050650@163.com>
2023-06-06 19:16:09 +0800

    mysql 和 redis 密码调整；配置文件修改

--- script/db/dbinit.sql
+++ script/db/dbinit.sql
@@ -1,6 +1,6 @@
 source /sql/cosd.sql
 source /sql/pool.sql
 
-CREATE USER 'cosd'@'%' IDENTIFIED BY 'cosd@123.com';
+CREATE USER 'cosd'@'%' IDENTIFIED BY 'bm5jenZpY3ppb3Z1Y3puZyA';
 GRANT ALL PRIVILEGES ON *.* TO 'cosd'@'%';
 FLUSH PRIVILEGES;
