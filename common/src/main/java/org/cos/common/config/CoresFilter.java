28d5c74085d9aa2a797394ea101bb75eca2c8e8a
魏冰涛 <13156050650@163.com>
2023-06-16 12:36:41 +0800

    跨域修改，只允许固定的ip和域名 访问服务器

--- common/src/main/java/org/cos/common/config/CoresFilter.java
+++ common/src/main/java/org/cos/common/config/CoresFilter.java
@@ -1,12 +1,13 @@
 package org.cos.common.config;
 
+import org.cos.common.exception.GlobalException;
+import org.cos.common.result.CodeMsg;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Arrays;
 
 @Component
@@ -24,16 +25,22 @@ public class CoresFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)  {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        String origin = request.getHeader("Access-Control-Allow-Origin");
+        String origin = request.getHeader("origin");
         if(Arrays.asList(domain.split(",")).contains(origin)){
             HttpServletResponse response = (HttpServletResponse) servletResponse;
             response.setHeader("Access-Control-Allow-Origin", origin);
             response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, HEAD");
             response.setHeader("Access-Control-Max-Age", "3600");
             response.setHeader("Access-Control-Allow-Headers", "access-control-allow-origin, authority, content-type, version-info, X-Requested-With");
-        }else{return;}
-       filterChain.doFilter(servletRequest, servletResponse);
+        }else{
+            throw  new GlobalException(CodeMsg.SERVER_ORIGIN_ERROR);
+        }
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } catch (Exception e) {
+            throw new GlobalException(CodeMsg.SERVER_ERROR);
+        }
     }
 }
