1554e3fd1f343c8c75bd4dd2cabcc354ac5c3ff4
魏冰涛 <13156050650@163.com>
2023-06-22 20:21:00 +0800

    测试类 Test1 中新增创建模版和删除模版的方法

--- application/src/test/java/Test1.java
+++ application/src/test/java/Test1.java
@@ -1,10 +1,17 @@
 import io.reactivex.disposables.Disposable;
 import org.apache.commons.lang3.StringUtils;
+import org.cos.application.CosdApplication;
+import org.cos.common.util.MailUtil;
 import org.cos.common.util.ZipUtils;
 import org.cos.common.util.crypt.CryptUtil;
 import org.junit.Test;
+import org.junit.jupiter.api.Order;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.web3j.abi.*;
 import org.web3j.abi.datatypes.Address;
 import org.web3j.abi.datatypes.Event;
@@ -26,6 +33,10 @@ import org.web3j.protocol.http.HttpService;
 import org.web3j.protocol.websocket.WebSocketService;
 import org.web3j.tx.Contract;
 import org.web3j.utils.Numeric;
+import software.amazon.awssdk.services.sesv2.SesV2Client;
+import software.amazon.awssdk.services.sesv2.model.CreateEmailTemplateRequest;
+import software.amazon.awssdk.services.sesv2.model.DeleteEmailTemplateRequest;
+import software.amazon.awssdk.services.sesv2.model.EmailTemplateContent;
 
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
@@ -45,7 +56,12 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CosdApplication.class)
 public class Test1 {
+    @Autowired
+    private SesV2Client sesV2Client;
     @Test
     public void test1(){
         Date date1 = new Date();
@@ -296,6 +312,207 @@ public class Test1 {
 //                    subscribe.dispose();
 //                }
 //            }
+    }
+    @org.junit.jupiter.api.Test
+    @Order(10)
+    public void createTemplate() {
+        String templateName = "VerifyCodeTemplate-English";
+        String subjectPart = "Verification - Chess of Stars";
+//        String textPart = "Dear {{name}},\nYour favorite animal is {{favoriteanimal}}.";
+        String textPart = "Dear user,\n" +
+                "We have received a request to authorize this email address for use with Chess of Stars. If you requested this verification, please enter the verification code {{code}} (valid for 5 minutes) to complete the operation.\n" +
+                "Note: This operation may modify your password, login email or bound phone number. If this operation was not initiated by you, please log in promptly and modify your password to ensure account security.\n" +
+                "Staff will never ask you for this verification code, do not disclose it!\n" +
+                "This is a system email, please do not reply. Please keep your email secure to prevent your account from being stolen by others.\n" +
+                "——Chess of Stars" ;
+        String htmlPart = "<!DOCTYPE html>\n" +
+                "<html lang=\"en\" xmlns:th=\"http://www.thymeleaf.org\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Verification - Chess of Stars</title>\n" +
+                "    <style>\n" +
+                "        table {\n" +
+                "            width: 700px;\n" +
+                "            margin: 0 auto;\n" +
+                "        }\n" +
+                "\n" +
+                "        #top {\n" +
+                "            width: 700px;\n" +
+                "            border-bottom: 1px solid #ccc;\n" +
+                "            margin: 0 auto 30px;\n" +
+                "        }\n" +
+                "\n" +
+                "        #top table {\n" +
+                "            font: 12px Tahoma, Arial, 宋体;\n" +
+                "            height: 40px;\n" +
+                "        }\n" +
+                "\n" +
+                "        #content {\n" +
+                "            width: 680px;\n" +
+                "            padding: 0 10px;\n" +
+                "            margin: 0 auto;\n" +
+                "        }\n" +
+                "\n" +
+                "        #content_top {\n" +
+                "            line-height: 1.5;\n" +
+                "            font-size: 14px;\n" +
+                "            margin-bottom: 25px;\n" +
+                "            color: #4d4d4d;\n" +
+                "        }\n" +
+                "\n" +
+                "        #content_top strong {\n" +
+                "            display: block;\n" +
+                "            margin-bottom: 15px;\n" +
+                "        }\n" +
+                "\n" +
+                "        #content_top strong span {\n" +
+                "            color: #f60;\n" +
+                "            font-size: 16px;\n" +
+                "        }\n" +
+                "\n" +
+                "        #verificationCode {\n" +
+                "            color: #f60;\n" +
+                "            font-size: 24px;\n" +
+                "        }\n" +
+                "\n" +
+                "        #content_bottom {\n" +
+                "            margin-bottom: 30px;\n" +
+                "        }\n" +
+                "\n" +
+                "        #content_bottom small {\n" +
+                "            display: block;\n" +
+                "            margin-bottom: 20px;\n" +
+                "            font-size: 12px;\n" +
+                "            color: #747474;\n" +
+                "        }\n" +
+                "\n" +
+                "        #bottom {\n" +
+                "            width: 700px;\n" +
+                "            margin: 0 auto;\n" +
+                "        }\n" +
+                "\n" +
+                "        #bottom div {\n" +
+                "            padding: 10px 10px 0;\n" +
+                "            border-top: 1px solid #ccc;\n" +
+                "            color: #747474;\n" +
+                "            margin-bottom: 20px;\n" +
+                "            line-height: 1.3em;\n" +
+                "            font-size: 12px;\n" +
+                "        }\n" +
+                "\n" +
+                "        #content_top strong span {\n" +
+                "            font-size: 18px;\n" +
+                "            color: #FE4F70;\n" +
+                "        }\n" +
+                "\n" +
+                "        #sign {\n" +
+                "            text-align: right;\n" +
+                "            font-size: 18px;\n" +
+                "            color: #FE4F70;\n" +
+                "            font-weight: bold;\n" +
+                "        }\n" +
+                "\n" +
+                "        #verificationCode {\n" +
+                "            height: 100px;\n" +
+                "            width: 680px;\n" +
+                "            text-align: center;\n" +
+                "            margin: 30px 0;\n" +
+                "        }\n" +
+                "\n" +
+                "        #verificationCode div {\n" +
+                "            height: 100px;\n" +
+                "            width: 680px;\n" +
+                "\n" +
+                "        }\n" +
+                "\n" +
+                "        .button {\n" +
+                "            color: #FE4F70;\n" +
+                "            margin-left: 10px;\n" +
+                "            height: 80px;\n" +
+                "            min-width: 80px;\n" +
+                "            resize: none;\n" +
+                "            font-size: 42px;\n" +
+                "            border: none;\n" +
+                "            outline: none;\n" +
+                "            padding: 10px 15px;\n" +
+                "            background: #ededed;\n" +
+                "            text-align: center;\n" +
+                "            border-radius: 17px;\n" +
+                "            box-shadow: 6px 6px 12px #cccccc,\n" +
+                "            -6px -6px 12px #ffffff;\n" +
+                "        }\n" +
+                "\n" +
+                "        .button:hover {\n" +
+                "            box-shadow: inset 6px 6px 4px #d1d1d1,\n" +
+                "            inset -6px -6px 4px #ffffff;\n" +
+                "        }\n" +
+                "\n" +
+                "    </style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<table>\n" +
+                "    <tbody>\n" +
+                "    <tr>\n" +
+                "        <td>\n" +
+                "            <div id=\"top\">\n" +
+                "                <table>\n" +
+                "                    <tbody><tr><td></td></tr></tbody>\n" +
+                "                </table>\n" +
+                "            </div>\n" +
+                "\n" +
+                "            <div id=\"content\">\n" +
+                "                <div id=\"content_top\">\n" +
+                "                    <strong>Dear User,</strong>\n" +
+                "                    <strong>\n" +
+                "                        We have received a request to authorize this email address for use with <span>Chess of Stars</span>. If you requested this verification, please enter the following verification code  <span>(valid for 5 minutes)</span> to complete the operation:\n" +
+                "                    </strong>\n" +
+                "                    <div id=\"verificationCode\">\n" +
+                "                        <!--                        <button class=\"button\" th:each=\"a:${code}\">[[${a}]]</button>-->\n" +
+                "                        <button class=\"button\" th:text=\"${code}\">{{code}}</button>\n" +
+                "                    </div>\n" +
+                "                </div>\n" +
+                "                <div id=\"content_bottom\">\n" +
+                "                    <small>\n" +
+                "                        Note: This operation may modify your password, login email or bound phone number. If this operation was not initiated by you, please log in promptly and modify your password to ensure account security.\n" +
+                "                        <br>(Staff will never ask you for this verification code, do not disclose it!)\n" +
+                "                    </small>\n" +
+                "                </div>\n" +
+                "            </div>\n" +
+                "            <div id=\"bottom\">\n" +
+                "                <div>\n" +
+                "                    <p>This is a system email, please do not reply.<br>\n" +
+                "                        Please keep your email secure to prevent your account from being stolen by others.\n" +
+                "                    </p>\n" +
+                "                    <p id=\"sign\">——Chess of Stars</p>\n" +
+                "                </div>\n" +
+                "            </div>\n" +
+                "        </td>\n" +
+                "    </tr>\n" +
+                "    </tbody>\n" +
+                "</table>\n" +
+                "</body>";
+
+        EmailTemplateContent templateContent = EmailTemplateContent.builder()
+                .subject(subjectPart)
+                .text(textPart)
+                .html(htmlPart)
+                .build();
+
+        CreateEmailTemplateRequest createEmailTemplateRequest = CreateEmailTemplateRequest.builder()
+                .templateName(templateName)
+                .templateContent(templateContent)
+                .build();
+        assertDoesNotThrow(() -> MailUtil.createTemplate(sesV2Client,createEmailTemplateRequest));
+        System.out.println("Test 4 passed");
+    }
+    @org.junit.jupiter.api.Test
+    @Order(9)
+    public void DeleteEmailTemplates() {
+        DeleteEmailTemplateRequest deleteEmailTemplateRequest = DeleteEmailTemplateRequest.builder()
+                .templateName("VerifyCodeTemplate")
+                .build();
+        assertDoesNotThrow(() -> MailUtil.deleteTemplate(sesV2Client,deleteEmailTemplateRequest));
+        System.out.println("Test 4 passed");
     }
 
 }
