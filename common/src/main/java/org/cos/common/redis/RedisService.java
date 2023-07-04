d9f99bb2b06dfb37d71f5714f0759fa04a57b392
魏冰涛 <13156050650@163.com>
2023-07-04 22:34:14 +0800

    RedisService 提交

--- common/src/main/java/org/cos/common/redis/RedisService.java
+++ common/src/main/java/org/cos/common/redis/RedisService.java
@@ -1,10 +1,14 @@
 package org.cos.common.redis;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.*;
+import redis.clients.jedis.params.XReadGroupParams;
+import redis.clients.jedis.params.XReadParams;
 
 import java.util.*;
 
@@ -13,6 +17,13 @@ public class RedisService {
 
 	@Autowired
 	JedisPool jedisPool;
+
+	@Value("${spring.redis.stream.key}")
+	private String streamKey;
+	@Value("${spring.redis.stream.group}")
+	private String streamGroup;
+	@Value("${spring.redis.stream.consumer}")
+	private String streamConsumer;
 
 	/**
 	 * 获取当个对象
@@ -407,7 +418,7 @@ public class RedisService {
 
 	/**
 	 * 获取指定键的所有属性（哈希）
-	 * @param key
+	 * @param
 	 * @return
 	 */
 	public Long publish(String channel, String message){
@@ -418,6 +429,166 @@ public class RedisService {
 		}finally {
 			returnToPool(jedis);
 		}
+	}
+
+	public <T> StreamEntryID xadd(KeyPrefix prefix, String key, T value) {
+		Jedis jedis = null;
+		try{
+			jedis =  jedisPool.getResource();
+			String realKey  = prefix.getPrefix() + key;
+			String content = beanToString(value);
+			Map<String,String> map = new HashMap();
+			map.put(realKey,content);
+			return jedis.xadd(streamKey, StreamEntryID.NEW_ENTRY, map);
+		}finally {
+			returnToPool(jedis);
+		}
+	}
+
+	public long xlen(String key) {
+		Jedis jedis = null;
+		try{
+			jedis =  jedisPool.getResource();
+			return jedis.xlen(key);
+		}finally {
+			returnToPool(jedis);
+		}
+
+	}
+
+	public String xgroupCreate(String key, String group, Boolean makeStream) {
+		Jedis jedis = null;
+		try{
+			jedis =  jedisPool.getResource();
+			return jedis.xgroupCreate(key, group,  StreamEntryID.LAST_ENTRY, makeStream);
+		}finally {
+			returnToPool(jedis);
+		}
+	}
+
+	public Map.Entry<String, List<StreamEntry>> xreadOne(XReadParams xReadParams, Map<String, StreamEntryID> streams) {
+		Jedis jedis = null;
+		try{
+			jedis =  jedisPool.getResource();
+			XReadGroupParams xReadGroupParams = new XReadGroupParams();
+			xReadGroupParams.count(1);
+			return jedis.xread(xReadParams, streams).get(0);
+		}finally {
+			returnToPool(jedis);
+		}
+
+	}
+
+	public List<Map.Entry<String, List<StreamEntry>>> xread(XReadParams xReadParams, Map<String, StreamEntryID> streams) {
+		Jedis jedis = null;
+		try{
+			jedis =  jedisPool.getResource();
+			return jedis.xread(xReadParams, streams);
+		}finally {
+			returnToPool(jedis);
+		}
+	}
+//	public <T> T get(KeyPrefix prefix, String key, Class<T> clazz) {
+
+	public <T>List<Map<StreamEntryID,T>> xreadGroup(KeyPrefix prefix, String key,  Class<T> clazz) {
+		Jedis jedis = null;
+		try{
+			jedis =  jedisPool.getResource();
+			List list = new ArrayList();
+			String realKey = prefix.getPrefix()+key;
+			// todo 每次读 10 个交易
+			XReadGroupParams xReadGroupParams = new XReadGroupParams().count(10);
+			Map<String, StreamEntryID> entry = new HashMap<>();
+			entry.put(streamKey, StreamEntryID.UNRECEIVED_ENTRY);
+			List<Map.Entry<String, List<StreamEntry>>> entries = jedis.xreadGroup(streamGroup, streamConsumer, xReadGroupParams, entry);
+			if (ObjectUtils.isNotEmpty(entries)&& entries.size()>0){
+				// todo 只读取 streamKey 下的交易
+				Map.Entry<String, List<StreamEntry>> stringListEntry = entries.get(0);
+				List<StreamEntry> value = stringListEntry.getValue();
+				for(StreamEntry streamEntry:value){
+					Map map = new HashMap();
+					Map<String, String> fields = streamEntry.getFields();
+					String s = fields.get(realKey);
+					T object = JSONObject.parseObject(fields.get(realKey), clazz);
+					map.put(streamEntry.getID(),object);
+					list.add(map);
+				}
+			}
+			return list;
+		}finally {
+			returnToPool(jedis);
+		}
+
+	}
+
+	public List<StreamEntry> xrange(String key, StreamEntryID start, StreamEntryID end, int count) {
+		Jedis jedis = null;
+		try{
+			jedis =  jedisPool.getResource();
+			return jedis.xrange(key, start, end, count);
+		}finally {
+			returnToPool(jedis);
+		}
+	}
+
+	public List<StreamEntry> xrevrange(String key, StreamEntryID start, StreamEntryID end, int count) {
+		Jedis jedis = null;
+		try{
+			jedis =  jedisPool.getResource();
+			return jedis.xrevrange(key, start, end, count);
+
+		}finally {
+			returnToPool(jedis);
+		}
+	}
+
+	public long xack(StreamEntryID... ids) {
+		Jedis jedis = null;
+		try{
+			jedis =  jedisPool.getResource();
+			return jedis.xack(streamKey, streamGroup, ids);
+
+		}finally {
+			returnToPool(jedis);
+		}
+	}
+
+	public long xdel(String key, StreamEntryID... ids) {
+		Jedis jedis = null;
+		try{
+			jedis =  jedisPool.getResource();
+			return jedis.xdel(key, ids);
+
+		}finally {
+			returnToPool(jedis);
+		}
+	}
+
+	public <T>List<Map<StreamEntryID,T>> xpending(KeyPrefix prefix, String key,  Class<T> clazz){
+		Jedis jedis = null;
+		try{
+			jedis =  jedisPool.getResource();
+			List list = new ArrayList();
+			String realKey = prefix.getPrefix()+key;
+			StreamEntryID start = new StreamEntryID(0, 0);
+			StreamEntryID end = new StreamEntryID(Long.MAX_VALUE, Long.MAX_VALUE);
+			// todo 一次性读取 10个未确认的交易
+			List<StreamPendingEntry> xpending = jedis.xpending(streamKey, streamGroup, start, end, 10, streamConsumer);
+			for ( StreamPendingEntry message : xpending) {
+				// todo 根据 消息id 读取消息详情
+				Map map = new HashMap<>();
+				List<StreamEntry> xrange = xrange(streamKey, message.getID(), message.getID(), 1);
+				Map<String, String> fields = xrange.get(0).getFields();
+				String s = fields.get(realKey);
+				T object = JSONObject.parseObject(fields.get(realKey), clazz);
+				map.put(message.getID(),object);
+				list.add(map);
+			}
+			return list;
+		}finally {
+			returnToPool(jedis);
+		}
+
 	}
 
 	private <T> String beanToString(T value) {
